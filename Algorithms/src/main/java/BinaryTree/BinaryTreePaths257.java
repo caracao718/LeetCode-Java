7acab112eaf45c324e14a310ba0b360de7004c5e
Cara Cao <caracaoengineer@gmail.com>
2022-03-25 21:37:42 -0700

    March 25 extra credit 3 easy binary tree

--- Algorithms/src/main/java/BinaryTree/BinaryTreePaths257.java
+++ Algorithms/src/main/java/BinaryTree/BinaryTreePaths257.java
@@ -4,19 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BinaryTreePaths257 {
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) {
-            this.val = val;
-        }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
 
         public List<String> binaryTreePaths(TreeNode root) {
             List<String> result = new ArrayList<>();
@@ -40,5 +27,5 @@ public class BinaryTreePaths257 {
                 searchBST(root.right, path + root.val + "->", result);
             }
         }
-    }
+
 }
